351dd6c731d6c3cfd75b180d7a37f552a29e3efa
West Kraemer <kraemerwest@gmail.com>
2019-02-21 17:51:45 -0500

    function wiuth param and string interp

---
+++ functionsAndSuch.playground/Contents.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+
+
+
+func getMilk(numberOfCartons: Double) {
+    print("Get \(numberOfCartons) cartons of milk")
+    var price = numberOfCartons * 3.50
+    print("the price is \(price)")
+}
+
+getMilk(numberOfCartons: 7)
+
+
+
+
+
