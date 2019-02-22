84a11cf0aa31600df990dc04220820733032d343
West Kraemer <kraemerwest@gmail.com>
2019-02-22 16:26:59 -0500

    add for in loop to function

--- functionsAndSuch.playground/Contents.swift
+++ functionsAndSuch.playground/Contents.swift
@@ -11,10 +11,18 @@ func getMilk(numberOfCartons: Int, howMuchMoneyGiven: Int) -> Int {
     return change
 }
 
-var getMilkVar = getMilk(numberOfCartons: 7, howMuchMoneyGiven: 30)
+var getMilkVar = getMilk(numberOfCartons: 7, howMuchMoneyGiven: 10)
 print("the change is \(getMilkVar)")
 
 
+func beerSong() {
+    var lyrics: String = ""
+    for number in 1...5 {
+        print("\(6 - number) bottles of beer on the wall, \(6 - number) bottles of beer, take one down, pass it around, \(number) bottles of beer on the wall.")
+    }
+}
+
+beerSong()
 
 
 
